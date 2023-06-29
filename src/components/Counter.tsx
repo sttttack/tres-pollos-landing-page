892a5be7d89318b6240de1a71296c9dcb2550127
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-06-29 13:58:12 -0700

    Counter remove

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,26 +1,26 @@
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 
-export default function Counter() {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter);
+// export default function Counter() {
+//   const dispatch = useDispatch();
+//   const count = useSelector((state) => state.counter);
 
-  const increment = () => {
-    dispatch({ type: "increment" });
-  };
-  const decrement = () => {
-    dispatch({ type: "decrement" });
-  };
+//   const increment = () => {
+//     dispatch({ type: "increment" });
+//   };
+//   const decrement = () => {
+//     dispatch({ type: "decrement" });
+//   };
 
-  const incbyfive = () => {
-    dispatch({ type: "increase", amount: 5 });
-  };
+//   const incbyfive = () => {
+//     dispatch({ type: "increase", amount: 5 });
+//   };
 
-  return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={increment}>Increase</button>
-      <button onClick={decrement}>Decrease</button>
-      <button onClick={incbyfive}>aa</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={increment}>Increase</button>
+//       <button onClick={decrement}>Decrease</button>
+//       <button onClick={incbyfive}>aa</button>
+//     </div>
+//   );
+// }
